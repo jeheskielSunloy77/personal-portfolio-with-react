bfbc9e155f9816b19642bac7134c54323fb019f3
Jeheskiel Sunloy <jeheskielventiokysunloy@gmail.com>
2022-07-15 23:27:16 +0700

    update data

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
 import { useContext } from "react"
 import { AppContext } from "./contexts/AppContext"
 import {
-	Loader,
-	Navbar,
-	About,
-	Footer,
-	Timeline,
-	Hero,
-	Intro,
-	SkillsnTools,
-	Projects,
-	Contact,
+  Loader,
+  Navbar,
+  About,
+  Footer,
+  Timeline,
+  Hero,
+  Intro,
+  SkillsnTools,
+  Projects,
+  Contact,
 } from "./components"
 import Contributions from "./components/Contributions"
 export default function App() {
-	const { loader } = useContext(AppContext)
-	return (
-		<>
-			<div className={!loader && "hidden"}>
-				<Loader />
-			</div>
-			<div className={loader && "hidden"}>
-				<Navbar />
-				<Hero />
-				<Intro />
-				<About />
-				<Timeline />
-				<SkillsnTools />
-				<Projects />
-				<Contributions />
-				<Contact />
-				<Footer />
-			</div>
-		</>
-	)
+  const { loader } = useContext(AppContext)
+  return (
+    <>
+      <div className={!loader && "hidden"}>
+        <Loader />
+      </div>
+      <div className={loader && "hidden"}>
+        <Navbar />
+        <Hero />
+        <Intro />
+        <About />
+        <Timeline />
+        <SkillsnTools />
+        <Projects />
+        <Contributions />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  )
 }
