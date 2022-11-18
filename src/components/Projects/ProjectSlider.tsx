6152be41d3759732439d884b4b9cb0168d2e5238
Jeheskiel Sunloy <jeheskielventiokysunloy@gmail.com>
2022-11-18 11:31:57 +0700

    update

--- src/components/Projects/ProjectSlider.tsx
+++ src/components/Projects/ProjectSlider.tsx
@@ -2,12 +2,7 @@ import SwiperCore, { Autoplay, Pagination } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/autoplay'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import crudAppImg from '../../assets/projects/crudApp.webp'
-import fapertaUnipaImg from '../../assets/projects/fapertaUnipa.webp'
-import iaknKupangImg from '../../assets/projects/iaknKupang.webp'
-import libraryImg from '../../assets/projects/library.webp'
-import memeGeneratorImg from '../../assets/projects/memeGenerator.webp'
-import weatherAppImg from '../../assets/projects/weatherApp.webp'
+import { projects } from '../../utils/constants'
 import ProjectCard from './ProjectCard'
 
 SwiperCore.use([Pagination, Autoplay])
@@ -27,109 +22,17 @@ const ProjectSlider = () => (
 			dynamicBullets: true,
 		}}
 	>
-		<div className='text-red-500'>
-			<SwiperSlide>
+		{projects.map((project, index) => (
+			<SwiperSlide key={index}>
 				<ProjectCard
-					title='Official Faculty Website'
-					desc='A Fullstack web app for Faculty of Argiculture, Papua University build with Next.js, Tailwindcss and Mongodb.'
-					img={fapertaUnipaImg}
-					details={{
-						status: 'Work on Progress',
-						code: 'Private',
-						demo: 'Prototype',
-						stack: 'Next.js, Mongodb, Auth0, Tailwindcss',
-					}}
-					links={{
-						demo: 'https://faperta-unipa-prototype.vercel.app/',
-					}}
+					title={project.title}
+					desc={project.desc}
+					img={project.img}
+					details={project.details}
+					links={project.links}
 				/>
 			</SwiperSlide>
-			<SwiperSlide>
-				<ProjectCard
-					title='Student Information System'
-					desc='An information system for IAKN Kupang university student, Build with Next.js, Postgresql, Supabase, and Tailwindcss.'
-					img={iaknKupangImg}
-					details={{
-						status: 'Work on Progress',
-						code: 'Private',
-						demo: 'Prototype',
-						stack: 'Next.js, Typescript, Postgresql, Supabase, Tailwindcss',
-					}}
-					links={{
-						demo: 'http://nextjs-iakn-kupang.vercel.app/',
-					}}
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<ProjectCard
-					title='CRUD App'
-					desc='A simple CRUD application with Antd on the frontend and using mockapi.io to handle the resources.'
-					img={crudAppImg}
-					details={{
-						status: 'Final',
-						code: 'Public',
-						demo: 'Production Available',
-						stack: 'ReactJS, Tailwindcss, Antd',
-					}}
-					links={{
-						demo: 'https://dataon-project.vercel.app/',
-						code: 'https://github.com/jeheskielSunloy77/dataon-project',
-					}}
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<ProjectCard
-					title='PWA Weather App'
-					desc='A progressive web application that can be installed on any device that can provide an acurate weather report from all around the world.'
-					img={weatherAppImg}
-					details={{
-						status: 'Stable',
-						code: 'Public',
-						demo: 'Production Available',
-						stack: 'Next.js, Typescript, Tailwindcss',
-					}}
-					links={{
-						demo: 'https://nextjs-weather-app-lemon.vercel.app/',
-						code: 'https://github.com/jeheskielSunloy77/nextjs-weather-app',
-					}}
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<ProjectCard
-					title='Fullstack Library'
-					desc='A full fledged library app, with CRUD functionality for administrators and a beautifull UI for users.'
-					img={libraryImg}
-					details={{
-						status: 'Work on Progress',
-						code: 'Public',
-						demo: 'Prototype',
-						stack: 'Next.js, Mongodb, Tailwindcss',
-					}}
-					links={{
-						demo: 'https://nextjs-fullstack-library-with-mongodb.vercel.app/',
-						code:
-							'https://github.com/jeheskielSunloy77/nextjs-fullstack-library-with-mongodb',
-					}}
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<ProjectCard
-					title='Meme Generator'
-					desc='A basic and simple website that can generate a random meme images and using user input to customize the meme.'
-					img={memeGeneratorImg}
-					details={{
-						status: 'Final',
-						code: 'Public',
-						demo: 'Available',
-						stack: 'ReactJS',
-					}}
-					links={{
-						demo: 'https://react-meme-generator-eta.vercel.app/',
-						code: 'https://github.com/jeheskielSunloy77/react-meme-generator',
-					}}
-				/>
-			</SwiperSlide>
-		</div>
+		))}
 	</Swiper>
 )
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,8 @@
+import crudAppImg from '../assets/projects/crudApp.webp'
+import fapertaUnipaImg from '../assets/projects/fapertaUnipa.webp'
+import iaknKupangImg from '../assets/projects/iaknKupang.webp'
+import weatherAppImg from '../assets/projects/weatherApp.webp'
+import webscraper from '../assets/projects/webscraper.webp'
 // import firebaseImg from '../../assets/icons/firebase.svg'
 // import javascriptImg from '../../assets/icons/javascript.svg'
 // import mongodbImg from '../../assets/icons/mongodb.svg'
@@ -31,6 +36,87 @@ export const cvLinks = {
 	english: '',
 	bahasa: '',
 }
+
+export const projects = [
+	{
+		title: 'Student Information System',
+		desc:
+			'An information system for IAKN Kupang university student, Build with Next.js, Postgresql, Supabase, and Tailwindcss.',
+		img: iaknKupangImg,
+		details: {
+			status: 'Work on Progress',
+			code: 'Private',
+			demo: 'Prototype',
+			stack: 'Next.js, Typescript, Postgresql, Supabase, Tailwindcss',
+		},
+		links: {
+			demo: 'http://nextjs-iakn-kupang.vercel.app/',
+		},
+	},
+	{
+		title: 'Ecommerce Web Scraper',
+		desc:
+			'A webscraper build with GO using serverless functions technology, that can scrape the web looking for any product from the user.',
+		img: webscraper,
+		details: {
+			status: 'Stable',
+			code: 'Public',
+			demo: 'Available',
+			stack: 'GO, Colly Framework',
+		},
+		links: {
+			demo: 'https://go-ecommerce-scraper.vercel.app/',
+			code: 'https://github.com/jeheskielSunloy77/go-ecommerce-scraper',
+		},
+	},
+	{
+		title: 'Official Faculty Website',
+		desc:
+			'A Fullstack web app for Faculty of Argiculture, Papua University build with Next.js, Tailwindcss and Mongodb.',
+		img: fapertaUnipaImg,
+		details: {
+			status: 'Work on Progress',
+			code: 'Private',
+			demo: 'Prototype',
+			stack: 'Next.js, Mongodb, Auth0, Tailwindcss',
+		},
+		links: {
+			demo: 'https://faperta-unipa-prototype.vercel.app/',
+		},
+	},
+	{
+		title: 'CRUD App',
+		desc:
+			'A simple CRUD application with Antd on the frontend and using mockapi.io to handle the resources.',
+		img: crudAppImg,
+		details: {
+			status: 'Final',
+			code: 'Public',
+			demo: 'Available',
+			stack: 'ReactJS, Tailwindcss, Antd',
+		},
+		links: {
+			demo: 'https://dataon-project.vercel.app/',
+			code: 'https://github.com/jeheskielSunloy77/dataon-project',
+		},
+	},
+	{
+		title: 'PWA Weather App',
+		desc:
+			'A progressive web application that can be installed on any device that can provide an acurate weather report from all around the world.',
+		img: weatherAppImg,
+		details: {
+			status: 'Stable',
+			code: 'Public',
+			demo: 'Available',
+			stack: 'Next.js, Typescript, Tailwindcss',
+		},
+		links: {
+			demo: 'https://nextjs-weather-app-lemon.vercel.app/',
+			code: 'https://github.com/jeheskielSunloy77/nextjs-weather-app',
+		},
+	},
+]
 
 // export const skillsNTools = [
 // 	{
