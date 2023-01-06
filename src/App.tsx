57127dba4a95ba03b46fed1d3a051e224eb5a9f0
Jeheskiel Sunloy <jeheskielventiokysunloy@gmail.com>
2023-01-06 22:08:11 +0700

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense, useContext } from 'react'
 import { Hero, Loader, Navbar } from './components'
 import { AppContext } from './utils/AppContext'
 // import background from "../assets/backgrounds/background.svg"
-const Intro = lazy(() => import('./components/Intro'))
+// const Intro = lazy(() => import('./components/Intro'))
 const About = lazy(() => import('./components/About'))
 const Timeline = lazy(() => import('./components/Timeline'))
 const SkillsnTools = lazy(() => import('./components/SkillsnTools'))
@@ -20,10 +20,13 @@ export default function App() {
 		<div className='bg-gradient-to-r from-gray-200 to-white dark:from-[#042c41] dark:to-black'>
 			{/* <div style={{ backgroundImage: `url(${background})` }}> */}
 			<Navbar />
-			<Hero />
-			<Suspense fallback={<span>Loading...</span>}>
+
+			<div className='from-gray-300 to-white dark:from-[#042c41] dark:to-black bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))]'>
+				<Hero />
+			</div>
+			{/* <Suspense fallback={<span>Loading...</span>}>
 				<Intro />
-			</Suspense>
+			</Suspense> */}
 			<Suspense fallback={<span>Loading...</span>}>
 				<About />
 			</Suspense>
