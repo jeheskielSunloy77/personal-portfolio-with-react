6678cdcdc2a4c8cd906296d63e8f894999525554
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-09-11 21:24:41 +0700

    fix: projects slider pagination not showing

--- src/components/Projects/ProjectSlider.tsx
+++ src/components/Projects/ProjectSlider.tsx
@@ -1,33 +1,31 @@
-import SwiperCore from 'swiper'
 import 'swiper/css'
-import 'swiper/css/autoplay'
+import 'swiper/css/pagination'
 import { Autoplay, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { projects } from '../../utils/constants'
 import ProjectCard from './ProjectCard'
 
-SwiperCore.use([Pagination, Autoplay])
-
-const ProjectSlider = () => (
-	<Swiper
-		className='w-fit'
-		effect='fade'
-		loop
-		autoplay={{
-			delay: 3000,
-			pauseOnMouseEnter: true,
-			disableOnInteraction: false,
-		}}
-		mousewheel={{ invert: false }}
-		autoHeight
-		pagination={{ dynamicBullets: true }}
-	>
-		{projects.map((_, index) => (
-			<SwiperSlide key={index}>
-				<ProjectCard projectIndex={index} projects={projects} />
-			</SwiperSlide>
-		))}
-	</Swiper>
-)
-
-export default ProjectSlider
+export default function ProjectSlider() {
+	return (
+		<Swiper
+			className='w-fit'
+			effect='fade'
+			loop
+			autoplay={{
+				delay: 3000,
+				pauseOnMouseEnter: true,
+				disableOnInteraction: false,
+			}}
+			modules={[Autoplay, Pagination]}
+			mousewheel={{ invert: false }}
+			autoHeight
+			pagination={{ dynamicBullets: true }}
+		>
+			{projects.map((_, index) => (
+				<SwiperSlide key={index}>
+					<ProjectCard projectIndex={index} projects={projects} />
+				</SwiperSlide>
+			))}
+		</Swiper>
+	)
+}
